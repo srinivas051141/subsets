d38b49a776b6acc5a99201694a53e8e48b273e47
srinivas051141 <54172230+srinivas051141@users.noreply.github.com>
2021-04-07 15:52:47 +0530

    Add files via upload

---
+++ subsets.py
@@ -0,0 +1,12 @@
+def sub_lists (l):
+    base = []  
+    lists = [base]
+    for i in range(len(l)):
+        orig = lists[:]
+        new = l[i]
+        for j in range(len(lists)):
+            lists[j] = lists[j] + [new]
+        lists = orig + lists
+          
+    return lists[1:];
+  
